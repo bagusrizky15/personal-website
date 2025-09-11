5d9a1944abe051f83c8dd2222f3929421f7c8cfe
 Bagus Rizky 🦊 <mohamad_rizky@bca.co.id>
2025-09-11 09:16:52 +0700

    add default theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
-        <ThemeProvider attribute="class" defaultTheme="light" disableSystemTheme>
+        <ThemeProvider attribute="class" defaultTheme="light">
           {children}
         </ThemeProvider>
       </body>
